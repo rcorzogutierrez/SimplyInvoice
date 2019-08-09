173cd530455d2e074a8dbb9e5b60d7f6c0348f66
Rafael Corzo <rcorzo@aeropost.com>
2019-08-09 17:29:03 -0400

    Adding List Invoices

--- src/app/components/invoices/invoice-list/invoice-list.component.ts
+++ src/app/components/invoices/invoice-list/invoice-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import {map} from 'rxjs/operators';
+import { InvoiceService } from '../../../services/invoice.service';
+import { IInvoice } from '../../../models/invoice';
+import { FormBuilder, FormArray } from '@angular/forms';
+import { PagerService } from '../../../services/pager.service';
 
 @Component({
   selector: 'app-invoice-list',
@@ -7,9 +12,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoiceListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private invoiceService: InvoiceService,private _fb: FormBuilder,private ref: ChangeDetectorRef,private pagerService: PagerService) { }
+  invoiceList: any[];
+  showDiv: true;
+  showEdit: true;
+  resultform: '';
+  summed;
+  searchText: string = "";
+  showDeletedMessage: boolean;
+  showUpdatedMessage:boolean;
+  private allItems: any[];
+   // pager object
+   pager: any = {};
+   // paged items
+   pagedItems: any[];
 
   ngOnInit() {
+    this.showDiv = true;
+
+    this.invoiceService.getInvoices().snapshotChanges().pipe(
+      map(data => data.map(datum => {
+        let purchases = datum.payload.toJSON()['purchases'];
+        return {
+          ...datum.payload.toJSON() as IInvoice,
+          $key: datum.key,
+          purchases: Object.keys(purchases).map(key => purchases[key])
+        }
+      })))
+      .subscribe(data => {        
+        this.invoiceList = data;
+        this.setPage(1);
+      });
+  }
+
+  onDelete($key: string) {   
+    this.invoiceService.deleteInvoice($key);
+    this.showDeletedMessage = true;
+      setTimeout(() => this.showDeletedMessage = false, 3000);
+  } 
+
+  removeDiv() {
+    this.showDiv = true;
+    this.showEdit = null;
+  }
+
+  filterCondition(customer) {  
+    return customer.customer.name.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1 || customer.customer.lastname.toLowerCase().indexOf(this.searchText.toLowerCase()) != -1;}
+
+  setPage(page: number) { 
+    if (page < 1 || page > this.pager.totalPages) {
+        return;
+    }
+    // get pager object from service
+    this.pager = this.pagerService.getPager(this.invoiceList.length, page);
+    // get current page of items
+    this.pagedItems = this.invoiceList.slice(this.pager.startIndex, this.pager.endIndex + 1);
   }
 
 }
