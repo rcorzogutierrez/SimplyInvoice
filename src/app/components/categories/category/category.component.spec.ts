d9a2cd84bb2232e53cc2f8f9cef9b52609e2d5c9
Rafael Corzo <rcorzo@aeropost.com>
2019-08-22 13:53:14 -0400

    continue testing bug fixed

--- src/app/components/categories/category/category.component.spec.ts
+++ src/app/components/categories/category/category.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { CategoryComponent } from './category.component';
 
@@ -8,6 +9,7 @@ describe('CategoryComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule ],
       declarations: [ CategoryComponent ]
     })
     .compileComponents();
